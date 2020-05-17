8aed27227e8fe9e1a5c6fb0a04e878c2c9d88acd
Alexis Susset <alexis_susset@icloud.com>
2020-05-17 11:21:56 +0800

    Cleanup of reconnect.sh script

--- src/reconnect.sh
+++ src/reconnect.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 # Script that tries to connect to google.com 
-curl -s --connect-timeout 52 http://google.com  > /dev/null
-if [[ $? != 0 ]]; then
+if curl -s --connect-timeout 52 http://google.com  > /dev/null; then
+	echo "Internet connection is working"
+else
     echo "Internet connection seems down, restarting device"
-    # Reset USB
+    # Reset USB power
     echo 0 > /sys/devices/platform/soc/3f980000.usb/buspower;
     sleep 5
     echo 1 > /sys/devices/platform/soc/3f980000.usb/buspower;
     sleep 5
 	if [[ -n "${CELLULAR_ONLY}" ]]; then
-		# Reset WiFi
-		ls /sys/class/net | grep -q wlan0
-		if [[ $? -eq 0 ]]; then
-			ifconfig wlan0 down
-		fi
-		ls /sys/class/net | grep -q eth0
-		if [[ $? -eq 0 ]]; then
-			ifconfig eth0 down
-		fi
+		# Reset WiFi and Ethernet
+		for interface in /sys/class/net/wlan*
+		do
+			[[ -e "${interface}" ]] || break # handle the case of no wlan interface
+			ifconfig "${interface}" down
+		done
+		for interface in /sys/class/net/eth*
+		do
+			[[ -e "$interface" ]] || break # handle the case of no wlan interface
+			ifconfig "${interface}" down
+		done
+		
 		sleep 5
-		ls /sys/class/net | grep -q wlan0
-		if [[ $? -eq 0 ]]; then
-			ifconfig wlan0 up
-		fi
-		ls /sys/class/net | grep -q eth0
-		if [[ $? -eq 0 ]]; then
-			ifconfig eth0 up
-		fi
+		for interface in /sys/class/net/wlan*
+		do
+			[[ -e "${interface}" ]] || break # handle the case of no wlan interface
+			ifconfig "${interface}" up
+		done
+		for interface in /sys/class/net/eth*
+		do
+			[[ -e "${interface}" ]] || break # handle the case of no wlan interface
+			ifconfig "${interface}" up
+		done
 		sleep 5
 	fi
     # Resin SUPERVISOR call to reboot the device
-    curl -X POST --header "Content-Type:application/json" "$RESIN_SUPERVISOR_ADDRESS/v1/reboot?apikey=$RESIN_SUPERVISOR_API_KEY"
-else
-	echo "Internet connection is working"
+    curl -X POST --header "Content-Type:application/json" "${RESIN_SUPERVISOR_ADDRESS}/v1/reboot?apikey=${RESIN_SUPERVISOR_API_KEY}"
 fi
